6881b55592234e9412d8dd0022abdcfdbcf4e00e
platoshkalekseevich <112236284+platoshkalekseevich@users.noreply.github.com>
2022-12-16 23:13:35 +0300

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+int main() {
+    int m, n;
+    std::cin >> n >> m;
+    if(n == 0 || m == 0)
+        std::cout << 0;
+    else {
+        int dp[m];
+        int value;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                std::cin >> value;
+                if (j == 0) {
+                    if (i == 0)
+                        dp[j] = value;
+                    else
+                        dp[j] = dp[j] + value;
+                } else {
+                    if (i == 0)
+                        dp[j] = value;
+                    else
+                        dp[j] = value + std::max(dp[j], dp[j - 1]);
+                }
+            }
+        }
+        std::cout << dp[n - 1];
+    }
+    return 0;
+}
